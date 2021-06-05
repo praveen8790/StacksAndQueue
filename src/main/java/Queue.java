c90d809e2b114f7f1f6267408f269445298e24aa
praveen8790 <praveen8978470998rock@gmail.com>
2021-06-05 10:09:17 +0530

    CREATED queue and ADDED method enqueue

---
+++ src/main/java/Queue.java
@@ -0,0 +1,16 @@
+import linkedlist.MyLinkedList;
+
+public class Queue<T extends Comparable<T>> {
+    MyLinkedList queue = new MyLinkedList();
+    public void enqueue(T data){
+        queue.append(data);
+    }
+
+    public static void main(String[] args) {
+        Queue myqueue = new Queue();
+        myqueue.enqueue(56);
+        myqueue.enqueue(30);
+        myqueue.enqueue(70);
+
+    }
+}
